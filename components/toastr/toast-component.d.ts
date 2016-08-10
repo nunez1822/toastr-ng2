554092bf84a2d12ed127342220bef850259aafae
Scott Cooper <scttcper@gmail.com>
2016-08-10 10:50:58 -0700

    Updates

--- components/toastr/toast-component.d.ts
+++ components/toastr/toast-component.d.ts
@@ -1,6 +1,7 @@
+import { OnDestroy } from '@angular/core';
 import { ToastConfig } from './toastr-config';
 import { ToastrService } from './toastr-service';
-export declare class Toast {
+export declare class Toast implements OnDestroy {
     private toastrService;
     toastId: number;
     timeout: number;
@@ -9,8 +10,13 @@ export declare class Toast {
     toastType: string;
     options: ToastConfig;
     state: string;
+    private hideTime;
+    private intervalId;
+    private width;
     constructor(toastrService: ToastrService);
+    ngOnDestroy(): void;
     activateToast(): void;
+    updateProgress(): void;
     tapToast(): void;
     remove(): void;
 }
--- components/toastr/toastr.d.ts
+++ components/toastr/toastr.d.ts
@@ -3,6 +3,5 @@ export { Overlay } from './overlay/overlay';
 export { OverlayContainer } from './overlay/overlay-container';
 export { OverlayRef } from './overlay/overlay-ref';
 export { Toast } from './toast-component';
-export { ToastProgress } from './progress-component';
 export { ToastrService, TOASTR_PROVIDERS, ActiveToast, ToastrModule } from './toastr-service';
 export { ToastrConfig, ToastConfig } from './toastr-config';
